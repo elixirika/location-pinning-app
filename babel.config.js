6858183a1c9ec1f59519648f0663a7fafa9fd599
elixirika <erikacarinoxx@gmail.com>
2024-11-08 15:23:29 +0100

    Configure env variables

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,15 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
-    'dotenv-import',
-        {
-          moduleName: '@env',
-          path: '.env',
-          safe: true,
-          allowUndefined: false
-        },
-    'react-native-reanimated/plugin'
+    [
+      'dotenv-import',
+      {
+        moduleName: '@env',
+        path: '.env',
+        safe: true,
+        allowUndefined: false,
+      },
+    ],
+    'react-native-reanimated/plugin',
   ],
 };
